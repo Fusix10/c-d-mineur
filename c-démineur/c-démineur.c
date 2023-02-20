bc3dd4e8704e3204215f46462ac178af7adaf0a5
Fusix10 <acamandona@gaming.tech>
2023-02-20 17:53:22 +0100

    open

--- c-démineur/c-démineur.c
+++ c-démineur/c-démineur.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
+#define n 10
+#define m 10
+
 int main()
 {
-    /*
-    int tab1[3] = { 1 ,2 ,3 };
-    int tab2[3] = { 1 ,2 ,3 };
-    int tab3[3] = { 1 ,2 ,3 };
-    */
-    int intu;
-    int compteur;
-    for (compteur = 0; compteur < 10; compteur++)
+    int grosTableau[n][m];
+    for (int i = 0; i < n; i++)
     {
-        for (intu = 0; intu < 10; intu++)
+        for (int j = 0; j < m; j++)
         {
-            printf("[]");
+            grosTableau[i][j] = 0;
+        }
+    }
+
+    grosTableau[5][3] = 1;
+    grosTableau[7][8] = 1;
+    grosTableau[0][0] = 1;
+   
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grosTableau[i][j] == 1)
+            {
+                printf("[x]");
+            }
+            else {
+                printf("[]");
+            }
         }
         printf("\n");
     }
