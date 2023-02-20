850221548d58b3db70b5f9530e08c5c038cbf24a
Fusix10 <acamandona@gaming.tech>
2023-02-20 17:13:22 +0100

    first

---
+++ c-démineur/c-démineur.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    int tab1[3] = { 1 ,2 ,3 };
+    int tab2[3] = { 1 ,2 ,3 };
+    int tab3[3] = { 1 ,2 ,3 };
+    */
+    int intu;
+    int compteur;
+    for (compteur = 0; compteur < 10; compteur++)
+    {
+        for (intu = 0; intu < 10; intu++)
+        {
+            printf("[]");
+        }
+        printf("\n");
+    }
+
+}
