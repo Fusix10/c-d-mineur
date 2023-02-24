b40cc0ee52f067b850e76bab83918faf8c3d25c7
William-Gabali <williamgabali@gmail.com>
2023-02-24 18:15:29 +0100

    c'est ici mais juste pas fini

--- c-démineur/c-démineur.c
+++ c-démineur/c-démineur.c
@@ -9,7 +9,7 @@ typedef struct Case{
     int visible;
     int bombe;
     int indice;
-    int drapeau;
+    int choix;
 } Case;
 
 void TryAdd1(Case grosTableau[n][m], int i, int j)
@@ -38,7 +38,7 @@ int main()
             grosTableau[i][j].bombe = 0;
             grosTableau[i][j].visible = 0;
             grosTableau[i][j].indice = 0;
-            grosTableau[i][j].drapeau = 0;
+            grosTableau[i][j].choix = 0;
         }
     }
     
@@ -69,23 +69,30 @@ int main()
     {
         for (int j = 0; j < m; j++)
         {
-            if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 0)
+            if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;)
             {
                 printf("[ ]");
             }
-            else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1) {
+            else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
                 printf("[X]");
             }
-            else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 1) {
+            else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
                 printf("[%d]",grosTableau[i][j].indice);
             }
-            else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0) {
+            else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;) {
                 printf("[ ]");
+            }
+            else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 1;) {
+                printf("[#]");
+            }
+            else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 1;) {
+                printf("[D]");
             }
         }
         printf("\n");
     }
     int winLose;
+    int* choix;
     winLose = 0;
     while (winLose !=1 || winLose !=0 ) {
         int x; int y;
@@ -94,66 +101,125 @@ int main()
         printf("quel coordonée voulez vous ?\n ");
         printf("x = "); scanf_s("%d", &x);
         printf(" y = "); scanf_s("%d", &y);
-        printf("Vous avez choisis x = %d y = %d\n", x, y);
+        printf("Vous avez choisis x = %d y = %d\ Que voulez-vous faire ?\n", x, y);
+        printf("\t1. Decouvrir une case\n");
+        printf("\t2. Placer un drapeau");
+        printf("\t\tVotre choix : ");
+        scanf_s("%d", &choix);
+        
+
+
         while (x >= 10 || y >= 10 || x < 0 || y < 0) {
             if (x >= n) {
                 printf("votre valeur x est supérieur ou égal a 10 il doit être inférieur");
                 printf("quel coordonée voulez vous ?\n ");
                 printf("x = "); scanf_s("%d", &x);
                 printf(" y = "); scanf_s("%d", &y);
-                printf("Vous avez choisis x = %d y = %d\n", x, y);
+                printf("Vous avez choisis x = %d y = %d\ Que voulez-vous faire ?\n", x, y);
+                printf("\t1. Decouvrir une case\n");
+                printf("\t2. Placer un drapeau");
+                printf("\t\tVotre choix : ");
+                scanf_s("%d", &choix);
             }
             else if (x < 0) {
                 printf("votre valeur x est inférieur a 0 il doit être égal ou supérieur");
                 printf("quel coordonée voulez vous ?\n ");
                 printf("x = "); scanf_s("%d", &x);
                 printf(" y = "); scanf_s("%d", &y);
-                printf("Vous avez choisis x = %d y = %d\n", x, y);
+                printf("Vous avez choisis x = %d y = %d\ Que voulez-vous faire ?\n", x, y);
+                printf("\t1. Decouvrir une case\n");
+                printf("\t2. Placer un drapeau");
+                printf("\t\tVotre choix : ");
+                scanf_s("%d", &choix);
             }
             else if (y >= m) {
                 printf("votre valeur y est supérieur ou égal a 10 il doit être inférieur");
                 printf("quel coordonée voulez vous ?\n ");
                 printf("x = "); scanf_s("%d", &x);
                 printf(" y = "); scanf_s("%d", &y);
-                printf("Vous avez choisis x = %d y = %d\n", x, y);
+                printf("Vous avez choisis x = %d y = %d\ Que voulez-vous faire ?\n", x, y);
+                printf("\t1. Decouvrir une case\n");
+                printf("\t2. Placer un drapeau");
+                printf("\t\tVotre choix : ");
+                scanf_s("%d", &choix);
             }
             else if (y < 0) {
                 printf("votre valeur y est inférieur a 0 il doit être égal ou supérieur");
                 printf("quel coordonée voulez vous ?\n ");
                 printf("x = "); scanf_s("%d", &x);
                 printf(" y = "); scanf_s("%d", &y);
-                printf("Vous avez choisis x = %d y = %d\n", x, y);
+                printf("Vous avez choisis x = %d y = %d\ Que voulez-vous faire ?\n", x, y);
+                printf("\t1. Decouvrir une case\n");
+                printf("\t2. Placer un drapeau");
+                printf("\t\tVotre choix : ");
+                scanf_s("%d", &choix);
             }
             else {
                 break;
             }
         }
         if (grosTableau[x][y].visible == 1) {
-            printf("tu a déja révéler se coup \n");
+            printf("tu a déja reveler se coup \n");
         }
         else if (grosTableau[x][y].visible == 0) {
             printf("new Revel \n");
             grosTableau[x][y].visible = 1;
         }
+        if (grosTableau[x][y].choix == 1) {
+             printf("tu place un drapeau ? du coup tu peut etre trouvé une bombe ?\n")
+                 for (int i = 0; i < n; i++)
+                 {
+                     for (int j = 0; j < m; j++)
+                     {
+                         grosTableau[i][j].visible = 1;
+                         if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;)
+                         {
+                             printf("[ ]");
+                         }
+                         else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
+                             printf("[X]");
+                         }
+                         else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
+                             printf("[%d]", grosTableau[i][j].indice);
+                         }
+                         else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;) {
+                             printf("[ ]");
+                         }
+                         else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 1;) {
+                             printf("[#]");
+                         }
+                         else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 1;) {
+                             printf("[D]");
+                         }
+
+                     }
+                 printf("\n");
+        }
         if (grosTableau[x][y].bombe == 1) {
-            printf("ta perdue CHEEEEEEH, you are dumbass noob (dumdum) \n");
+            printf("BOOM t'a perdue dommage, \n");
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
                     grosTableau[i][j].visible = 1;
-                    if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 0)
+                    if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;)
                     {
                         printf("[ ]");
                     }
-                    else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1) {
+                    else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
                         printf("[X]");
                     }
-                    else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 1) {
+                    else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
                         printf("[%d]", grosTableau[i][j].indice);
                     }
-                    else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0) {
+                    else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;) {
                         printf("[ ]");
+                    }
+                    else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 1;) {
+                        printf("[#]");
+                    }
+                    else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 1;) {
+                        printf("[D]");
                     }
 
                 }
@@ -162,24 +228,30 @@ int main()
             break;
         }
         else if (grosTableau[x][y].bombe == 0) {
-            printf("chanceux je pari ta fait au pif \n");
+            printf("pas de bombe ici \n");
         }
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
-                if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 0)
+                if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;)
                 {
                     printf("[ ]");
                 }
-                else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1) {
+                else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
                     printf("[X]");
                 }
-                else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 1) {
+                else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 0;) {
                     printf("[%d]", grosTableau[i][j].indice);
                 }
-                else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0) {
+                else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 0;) {
                     printf("[ ]");
+                }
+                else if (grosTableau[i][j].bombe == 0 && grosTableau[i][j].visible == 0 && grosTableau[i][j].choix == 1;) {
+                    printf("[#]");
+                }
+                else if (grosTableau[i][j].bombe == 1 && grosTableau[i][j].visible == 1 && grosTableau[i][j].choix == 1;) {
+                    printf("[D]");
                 }
                 
             }
